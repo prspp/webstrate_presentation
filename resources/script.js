0b669630e5bfed5cdc3737e7266b17481d055aad
Manuela-AYO <manuelaayo7@gmail.com>
2024-05-24 19:56:32 +0200

    Iframe instead of div

--- resources/script.js
+++ resources/script.js
@@ -1,5 +1,5 @@
 webstrate.on("loaded", function(webstrateId) {
-    var slideShow = document.getElementById("main");
+    var slideShow;
     var slidesStructure = document.getElementById("slidesStructure");
     var slidesNotes = document.getElementById("slidesNotes");
     var newBlock = document.getElementById("newSlide");
@@ -9,7 +9,96 @@ webstrate.on("loaded", function(webstrateId) {
     let currentZIndex = 1, fontSize=16, brushColor = "#000", brushThickness = 2;
     var activeSlideInMain, isDrawing=false, isDrawingMode=false;
     var titleStyle = false;
-    var styleEnabled;
+    var iframe = document.getElementById("main");
+
+    // create list node
+    function createListNode(outlineDiv, outline, nodeProperty) {
+        if(!outline) {
+            outline = document.createElement("ol");
+            outline.className = "listTitles";
+            outlineDiv.appendChild(outline);
+        }
+
+        // add text content
+        var listItem = document.createElement("li");
+        var paragraph = document.createElement("p");
+        paragraph.classList.add(activeSlideInMain.classList[0]);
+        paragraph.classList.add(nodeProperty.classList[nodeProperty.classList.length-1]);
+        if(outline.id == "outline") {
+            paragraph.addEventListener("click", (event) => {
+                const slideClass = event.target.classList[0];
+                const slide = document.getElementById("slides").querySelector(slideClass);
+                document.getElementById("active").removeAttribute("id");
+                slide.id = "active";
+                showActiveSlide();
+            });
+        }
+        paragraph.textContent = nodeProperty.textContent;
+        listItem.appendChild(paragraph);
+        outline.appendChild(listItem);
+    }
+
+    // create outline item
+    function createElementInOutline(textBox) {
+        var outlineDiv = document.getElementById("outline");
+        outlineDiv.hidden = true;
+        var outline = outlineDiv.querySelector(".listTitles");
+        createListNode(outlineDiv, outline, textBox);
+        createElementInNote(textBox);
+        outlineDiv.removeAttribute("hidden");
+    }
+
+    // create the outline item in note
+    function createElementInNote(textBox) {
+        slidesNotes.hidden = true;
+        var outline = slidesNotes.querySelector(".listTitles");
+        createListNode(slidesNotes, outline, textBox);
+        slidesNotes.removeAttribute("hidden");
+    }
+
+    // update outline
+    const updateOutline = (event) => {
+        const callingTitleClass = event.target.classList[event.target.classList.length-1];
+        var outlineDiv = document.getElementById("outline");
+        var paragraph = outlineDiv.querySelector(".listTitles").getElementsByClassName(callingTitleClass)[0];
+        paragraph.innerHTML = event.target.innerHTML;
+        updateNotes(event, callingTitleClass);
+    }
+
+    // update notes
+    const updateNotes = (event, callingTitleClass) => {
+        var slidesDiv = document.getElementById("slidesNotes");
+        var paragraph = slidesDiv.querySelector(".listTitles").getElementsByClassName(callingTitleClass)[0];
+        paragraph.innerHTML = event.target.innerHTML;
+    }
+
+    // add title
+    function addTextStyle() {
+        if(!activeSlideInMain) return;
+        const titleBox = document.createElement("p");
+        const nbTitles = document.getElementsByClassName("title").length;
+        titleBox.classList.add("title");
+        titleBox.classList.add("outlined");
+        titleBox.classList.add("title-" + nbTitles.toString());
+        titleBox.style.top = "10px";
+        titleBox.style.left = "50px";
+        titleBox.style.width = "200px";
+        titleBox.setAttribute("contentEditable", "");
+        titleBox.style.zIndex = currentZIndex ++;
+        titleBox.textContent = "Add title";
+        titleBox.style.fontSize = "50px";
+        titleBox.style.fontWeight = "bold";
+        titleBox.addEventListener("focus", () => {
+            titleBox.classList.remove("outlined");
+        });
+        titleBox.addEventListener("input", updateOutline);
+        titleBox.addEventListener("paste", updateOutline);
+        titleBox.addEventListener("cut", updateOutline);
+        titleBox.addEventListener("delete", updateOutline);
+        activeSlideInMain.appendChild(titleBox);
+        setupDragEvents(titleBox);
+        createElementInOutline(titleBox);
+    }
 
     // add text box
     function addTextBox() {
@@ -57,10 +146,6 @@ webstrate.on("loaded", function(webstrateId) {
                 {
                     element.style.left = current_left + "px";
                     element.style.top = current_top + "px";
-                    console.log("Satisfied");
-                }
-                else {
-                    console.log("Not satisfied");
                 }
             };
 
@@ -76,6 +161,7 @@ webstrate.on("loaded", function(webstrateId) {
 
     // show the active slide
     function showActiveSlide() {
+        if(!slideShow) return;
         // get the element having the active class
         var activeSlide = document.getElementById("active");
 
@@ -83,14 +169,15 @@ webstrate.on("loaded", function(webstrateId) {
         var className = activeSlide.className;
 
         // among the children of main, display the appropriate div
-        activeSlideInMain = document.querySelector("." + className + ".slide");
+        console.log(slideShow);
+        activeSlideInMain = slideShow.querySelector("." + className + ".slide");
         
         if(activeSlideInMain) {
             // set his attribute hidden to false
             activeSlideInMain.removeAttribute("hidden");
 
             // hide the other divs
-            let children = document.getElementsByClassName("slide");
+            let children = slideShow.getElementsByClassName("slide");
             
             for(let i=0; i<children.length; i++) {
                 if(!children[i].className.includes(className)) {
@@ -116,7 +203,6 @@ webstrate.on("loaded", function(webstrateId) {
         let children = document.getElementsByClassName("slideNote");
         for(let i=0; i<children.length; i++) {
             // hide the other divs notes
-            console.log(children[i]);
             if(!classNames.includes(children[i].className[0])) {
                 children[i].setAttribute("hidden", "");
             }
@@ -128,7 +214,7 @@ webstrate.on("loaded", function(webstrateId) {
         }
 
         // print slide title
-        document.getElementById("slideTitle").innerText = "Slide " + slideNumber.toString();
+        document.getElementById("slideTitle").innerText = "Slide " + (slideNumber + 1).toString();
         
         // repaint
         slidesNotes.removeAttribute("hidden");
@@ -138,7 +224,7 @@ webstrate.on("loaded", function(webstrateId) {
     function createNewSlide() {
         // count the current number of p element in slides
         var allSlides = document.getElementById("slides");
-        var nbSlidesCreated = allSlides.childElementCount - 1;
+        var nbSlidesCreated = allSlides.childElementCount;
 
         /* ___ on the slide show ___ */
 
@@ -172,6 +258,7 @@ webstrate.on("loaded", function(webstrateId) {
 
         // set it as the main slide
         activeSlideInMain = slideShow.lastChild;
+        console.log("Active : ", activeSlideInMain);
 
         /* ___ on the notes ___ */
 
@@ -198,10 +285,6 @@ webstrate.on("loaded", function(webstrateId) {
         newNotes.className = "slide-" + nbSlidesCreated.toString() + " slideNote";
         newNotes.setAttribute("contenteditable", "");
         slidesNotes.appendChild(newNotes);
-    
-        if(titleStyle) {
-            handleTitleType(styleEnabled);
-        } 
     }
 
     // show the selected slide
@@ -266,120 +349,24 @@ webstrate.on("loaded", function(webstrateId) {
         }
     }
 
-    // define the title's style of the slide
-    function handleTitleType(styleClicked) {
-        if(!titleStyle) {
-            // remove the active class name if present
-            const parent = document.getElementById("toolBar");
-            const child = parent.getElementsByClassName("active");
-            if(child.length > 0) {
-                child[0].removeAttribute("class");
-            }
-            return;
-        }
-
-        // get the id of the paragraph clicked
-        styleEnabled = styleClicked;
-
-        const list_elements = ["ul", "ol"];
-        let previousSibling;
-
-        if(styleClicked == "activateTitle") {
-            // assign the clicked element the class active
-            styleTitle.className = "active";
-
-            // check if the active slide is already in a list
-            var parent = activeSlideInMain.parentElement;
-            var parentName = parent.nodeName.toLowerCase();
-            
-            if(activeSlideInMain.previousElementSibling) {
-                previousSibling = activeSlideInMain.previousElementSibling.nodeName.toLowerCase();
-            }
-            else {
-                previousSibling = "None";
-            }
-
-            // if not, put it in a list
-            if(parentName == "div" && !list_elements.includes(previousSibling)) {
-                // insert it at the main slide position
-                activeSlideInMain.insertAdjacentHTML("beforebegin", `<ol></ol>`);
-
-                // do the same in the personal notes
-                activeSlideNote.insertAdjacentHTML("beforebegin", `<ol></ol>`);
-            }
-            
-            // get the list - the variable name parent is used here to show that this list tag 
-            // will be its new parent
-            parent = activeSlideInMain.previousElementSibling;
-            var parentInNotes = activeSlideNote.previousElementSibling;
-
-            parent.insertAdjacentHTML("beforeend", "<li></li>");
-            parentInNotes.insertAdjacentHTML("beforeend", `<li></li>`);
-
-            // insert the slide in the list
-            var listItem = parent.lastChild;
-            listItem.appendChild(activeSlideInMain); 
-
-            var listItemInNote = parentInNotes.lastChild;
-            listItemInNote.appendChild(activeSlideNote);
-        }
-    }
-
-    function init() {
-        // add the click listener to the slide notes in the document
-        for(const child of slidesStructure.children) {
-            child.addEventListener("click", (event) => { toggleSlideNoteState(event.target) });
-        }
-
+    // add listeners to iframe elements
+    function addListenerIframe(document) {
         // add the drag events to the canvas in the document
-        document.querySelectorAll(".drawing-canvas").forEach(element => {
+        document.body.querySelectorAll(".drawing-canvas").forEach(element => {
             setupDragEvents(element);
         });
 
         // do the same for the text boxes
-        document.querySelectorAll(".text-box").forEach(element => {
+        document.body.querySelectorAll(".text-box").forEach(element => {
             setupDragEvents(element);
         });
+    }
 
-        
-        // add the click listener to the slides
-        const slides = document.querySelectorAll("#slides p");
-        for(const slide of slides) {
-            if(slide.id != "newSlide") {
-                console.log(slide);
-                slide.addEventListener("click", (event) => {
-                    toggleSlideShow(event.target);
-                    showActiveSlide();
-                });
-            }
-        }
-
-        // handle the new slide creation event
-        newBlock.addEventListener("click", () => {
-            // create a new slide
-            createNewSlide();
-
-            // show the slide
-            showActiveSlide();
-        });
-
-        // check if a title/subtitle style is applied
-        const parent = document.getElementById("toolBar");
-        const child = parent.getElementsByClassName("active");
-        if(child.length > 0) {
-            titleStyle = true;
-            styleEnabled = child[0].id;
-        }
-
+    // add listeners to toolbar buttons
+    function addToolbarListeners() {
         // handle the style events
-        styleTitle.addEventListener("click", (event) => {
-            toggleTitleState();
-            handleTitleType(event.target.id);
-        });
-
-        styleSubtitle.addEventListener("click", (event) => {
-            toggleTitleState();
-            handleTitleType(event.target.id);
+        styleTitle.addEventListener("click", () => {
+            addTextStyle();
         });
 
         // add listeners to buttons
@@ -401,9 +388,57 @@ webstrate.on("loaded", function(webstrateId) {
             brushThickness = event.target.value;
         });
         document.getElementById("toggleDrawingModeBtn").addEventListener("click", toggleDrawingMode);
-
-        showActiveSlide();
     }
+
+    function init() {
+        console.log("Here first");
+        // add the click listener to the slide notes in the document
+        for(const child of slidesStructure.children) {
+            child.addEventListener("click", (event) => { toggleSlideNoteState(event.target) });
+        }
+
+        // add the click listener to the slides
+        const slides = document.querySelectorAll("#slides p");
+        for(const slide of slides) {
+            if(slide.id != "newSlide") {
+                slide.addEventListener("click", (event) => {
+                    toggleSlideShow(event.target);
+                    showActiveSlide();
+                });
+            }
+        }
+
+        // handle the new slide creation event
+        newBlock.addEventListener("click", () => {
+            // create a new slide
+            createNewSlide();
+
+            // show the slide
+            showActiveSlide();
+        });
+    }
+
+    iframe.webstrate.on("transcluded", () => {
+        var innerDoc = iframe.contentWindow.document;
+        if(!innerDoc.style) {
+            var link = innerDoc.createElement("style");
+            link.id = "style.css";
+            innerDoc.head.appendChild(link);
+        }
+        if(innerDoc.getElementsByClassName("slide").length == 0) {
+            var div = innerDoc.createElement("div");
+            div.classList.add("slide-1");
+            div.classList.add("slide");
+            var canvas = innerDoc.createElement("canvas");
+            canvas.className = "drawing-canvas";
+            div.appendChild(canvas);
+            innerDoc.body.appendChild(div);
+        }
+        addListenerIframe(innerDoc);
+        addToolbarListeners();
+        slideShow = innerDoc.body;
+        showActiveSlide();
+    });
 
     init();
 });
